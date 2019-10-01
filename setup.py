a87600ac639975fb342062c0afbd240b76a91ef5
eldelahoz <andelahoz13@gmail.com>
2019-10-01 13:44:15 -0500

    Paquete Distribuible agregado

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PaqueteCalculos",
+    version="0.1",
+    description="Paquete de redondeo y potencia",
+    author="Andres De la Hoz",
+    author_email="andelahoz13@gmail.com",
+    url="localhost",
+    packages=["calculos", "calculos.redondeo_potencia"]
+)

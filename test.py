c2b003c1ae5fdbcb8502531b2c50a468227e0aa7
eldelahoz <andelahoz13@gmail.com>
2019-10-01 13:27:23 -0500

    Mitad de Paquetes

--- test.py
+++ test.py
@@ -1,26 +1,5 @@
-email = input("Introduce una direccion de email: ")
+from calculos.calculos_generales import dividir, potencia
 
+dividir(10, 2)
 
-def repetir_email():
-    pass
-
-
-while email.find("@") == 0:
-    print("No puedes poner la @ al inicio")
-    print("Tienes que ingresarla corectamente".center(50))
-    email = input("Introduce una direccion de email:")
-while email.find("@") == len(email) - 1:
-    print("No se puede poner la @ a lo ultimo")
-    print("Tienes que ingresarla corectamente".center(50))
-    email = input("Introduce una direccion de email:")
-while not "@" in email.lower():
-    print("No hay @ en tu direccion de email")
-    print("Tienes que ingresarla corectamente".center(50))
-    email = input("Introduce una direccion de email:")
-while email.count("@") != 1:
-    print("Hay mas de una @")
-    print("Tienes que ingresarla corectamente".center(50))
-    email = input("Introduce una direccion de email:")
-
-print("Tu direccion es corecta: {}".format(email))
-
+potencia(4, 6)

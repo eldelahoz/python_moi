b80574fde4d7d87cf0a4f8695bbbca8c3a3b581d
eldelahoz <andelahoz13@gmail.com>
2019-10-08 16:33:29 -0500

    Interfaces_Graphique Calculatrice Fin

--- Interfaces_graphiques/interface_calculatrice.py
+++ Interfaces_graphiques/interface_calculatrice.py
@@ -42,9 +42,10 @@ def SuprEcran():
     Ecran.delete(0, END)
     NombreEcran.set("0")
     Result = 0
+    operation = ""
 
 
-def SumEcran(num):
+def AdditionEcran(num):
     global operation, Result, Reset_ecran
 
     Result += int(num)
@@ -54,7 +55,7 @@ def SumEcran(num):
     NombreEcran.set(Result)
 
 
-def RestEcran(nomb):
+def SoustractionEcran(nomb):
     global operation, Result, Reset_ecran
     if Result == 0:
         Result = int(nomb)
@@ -65,12 +66,47 @@ def RestEcran(nomb):
     NombreEcran.set(Result)
 
 
+def MultiplicationEcran(nomb):
+    global operation, Result, Reset_ecran
+    if Result == 0:
+        Result = int(nomb)
+    else:
+        Result *= int(nomb)
+    operation = "multiplication"
+    Reset_ecran = True
+    NombreEcran.set(Result)
+
+
+def DivisionEcran(nomb):
+    global operation, Result, Reset_ecran
+    if Result == 0:
+        Result = int(nomb)
+    else:
+        Result /= int(nomb)
+    operation = "division"
+    Reset_ecran = True
+    NombreEcran.set(Result)
+
+
 def Le_Result():
     global operation
     global Result
     if operation == "suma":
         NombreEcran.set(Result + int(NombreEcran.get()))
         Result = 0
+        operation = ""
+    elif operation == "resta":
+        NombreEcran.set(Result - int(NombreEcran.get()))
+        Result = 0
+        operation = ""
+    elif operation == "multiplication":
+        NombreEcran.set(Result * int(NombreEcran.get()))
+        Result = 0
+        operation = ""
+    elif operation == "division":
+        NombreEcran.set(Result / int(NombreEcran.get()))
+        Result = 0
+        operation = ""
 
 
 # <editor-fold desc="File1">
@@ -80,7 +116,7 @@ Button8 = Button(MonFrame, text="8", width=3, command=lambda: NombrePul("8"))
 Button8.grid(row=2, column=2)
 Button9 = Button(MonFrame, text="9", width=3, command=lambda: NombrePul("9"))
 Button9.grid(row=2, column=3)
-ButtonDiv = Button(MonFrame, text="/", width=3)
+ButtonDiv = Button(MonFrame, text="/", width=3, command=lambda: DivisionEcran(NombreEcran.get()))
 ButtonDiv.grid(row=2, column=4)
 # </editor-fold>
 
@@ -91,7 +127,7 @@ Button5 = Button(MonFrame, text="5", width=3, command=lambda: NombrePul("5"))
 Button5.grid(row=3, column=2)
 Button6 = Button(MonFrame, text="6", width=3, command=lambda: NombrePul("6"))
 Button6.grid(row=3, column=3)
-ButtonMul = Button(MonFrame, text="X", width=3)
+ButtonMul = Button(MonFrame, text="X", width=3, command=lambda: MultiplicationEcran(NombreEcran.get()))
 ButtonMul.grid(row=3, column=4)
 # </editor-fold>
 
@@ -102,18 +138,18 @@ Button2 = Button(MonFrame, text="2", width=3, command=lambda: NombrePul("2"))
 Button2.grid(row=4, column=2)
 Button3 = Button(MonFrame, text="3", width=3, command=lambda: NombrePul("3"))
 Button3.grid(row=4, column=3)
-ButtonRest = Button(MonFrame, text="-", width=3, command=lambda: RestEcran(NombreEcran.get()))
+ButtonRest = Button(MonFrame, text="-", width=3, command=lambda: SoustractionEcran(NombreEcran.get()))
 ButtonRest.grid(row=4, column=4)
 # </editor-fold>
 
 # <editor-fold desc="File4">
 Button0 = Button(MonFrame, text="0", width=3, command=lambda: NombrePul("0"))
 Button0.grid(row=5, column=1)
-ButtonComa = Button(MonFrame, text=",", width=3, command=lambda: NombrePul(","))
+ButtonComa = Button(MonFrame, text=".", width=3, command=lambda: NombrePul("."))
 ButtonComa.grid(row=5, column=2)
 ButtonIgual = Button(MonFrame, text="=", width=3, command=lambda: Le_Result())
 ButtonIgual.grid(row=5, column=3)
-ButtonSuma = Button(MonFrame, text="+", width=3, command=lambda: SumEcran(NombreEcran.get()))
+ButtonSuma = Button(MonFrame, text="+", width=3, command=lambda: AdditionEcran(NombreEcran.get()))
 ButtonSuma.grid(row=5, column=4)
 # </editor-fold>
 
